7b503ffac5e43ce38c60ce3dfbd7e17e7dcefb99
Sue <suet@inoxico.com>
2022-01-07 10:16:43 +0200

    Domain Event Publisher V1

---
+++ Distro.Domain.Common/DomainEventPublisher.cs
@@ -0,0 +1,44 @@
+﻿namespace Distro.Domain.Common
+{
+    public class DomainEventPublisher
+    {
+        private static IDomainEventDispatcher _instance;
+        public static void SetInstance(IDomainEventDispatcher instance)
+        {
+            _instance = instance; 
+        }
+        public static IDomainEventDispatcher Instance
+        {
+            get => _instance;
+            private set => _instance = value;
+        }
+    }
+    
+    public interface IDomainEventDispatcher
+    {
+        void Publish<T>(T @event) where T : IDomainEvent;
+        
+        void Send<T>(T command)where T : IDomainCommand;
+
+        TResponse Request<TRequest, TResponse>(TRequest request) where TRequest : IDomainRequest;
+    }
+
+    public interface IDomainEvent { }
+    public interface IDomainCommand { }
+    public interface IDomainRequest { }
+    
+    public interface IDomainEventHandler<T> where T : IDomainEvent
+    {
+        void Handle(T @event);
+    }
+    
+    public interface IDomainCommandHandler<T> where T : IDomainCommand
+    {
+        void Handle(T @event);
+    }
+    
+    public interface IDomainRequestHandler<TRequest, TResponse> where TRequest : IDomainRequest
+    {
+        TResponse Handle(TRequest request);
+    }
+}
